fa4eaf1a57022ae9aec1825c02fdab76ebebe079
vboxuser <valmalle.tom@gmail.com>
2025-12-04 16:40:17 +0100

    ert

--- sample-app.sh
+++ sample-app.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-mkdir tempdir
-mkdir tempdir/templates
-mkdir tempdir/static
+mkdir -p tempdir/templates
+mkdir -p tempdir/static
 
 cp sample_app.py tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
 
-cat << EOF > /tempdir/Dockerfile
+cat << EOF > tempdir/Dockerfile
 FROM python
 RUN pip install flask
-COPY  ./static /home/myapp/static/
-COPY  ./templates /home/myapp/templates/
-COPY  sample_app.py /home/myapp/
+COPY ./static /home/myapp/static/
+COPY ./templates /home/myapp/templates/
+COPY sample_app.py /home/myapp/
 EXPOSE 5050
 CMD python /home/myapp/sample_app.py
 EOF
@@ -21,4 +20,4 @@ EOF
 cd tempdir
 docker build -t sampleapp .
 docker run -t -d -p 5050:5050 --name samplerunning sampleapp
-docker ps -a 
+docker ps -a
